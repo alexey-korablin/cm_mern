14cad619b33126950d9850523d348356d155e0bb
Alexey Korablin <Alexey_Korablin@epam.com>
2020-03-12 15:23:03 +0300

    08_be Code formatting

--- server/index.js
+++ server/index.js
@@ -13,14 +13,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
-db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+db.on(
+  'error',
+  console.error.bind(console, 'MongoDB connection error: '),
+);
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+  res.send('Hello world');
 });
 
 // Add middleware on route '/api'. The moddleware is movie router
 app.use('/api', movieRouter);
 
-app.listen(apiPort, () => { 
-    console.log(`Server running on port ${apiPort}`) });
+app.listen(apiPort, () => {
+  console.log(`Server running on port ${apiPort}`);
+});
